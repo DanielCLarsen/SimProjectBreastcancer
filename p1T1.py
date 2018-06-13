33e7973b95e960f06820880dc470615918537eff
Daniel Cohen Larsen <daniel.clarsen@gmail.com>
2018-06-13 12:36:08 +0200

    ss

--- p1T1.py
+++ p1T1.py
@@ -11,5 +11,25 @@ P = np.matrix([[0.9915,0.005,0.0025,0,0.001],
                [0,0,0,0.991,0.009],
                [0,0,0,0,1]])
 #1 timestep = 1 month; sim 1000 women; start state 1;
+cdf=np.zeros((len(P),len(P)+1))
+for i in range(0,len(P)):
+    for j in range(1,len(P)+1):
+        cdf[i,j] = cdf[i,j-1]+P[i,j-1]
+
+
+'''
+cdf[0]=0
+for i in range(1,7):
+    cdf[i]=cdf[i-1]+pdf[i-1]
+
+xCrude = [0]*outcomes
+for i in range(0, outcomes):
+    for j in range(0,6):
+        if cdf[j]<U[i] and cdf[j+1] >= U[i]:
+            xCrude[i]=j+1
+'''
+
+
 #Until death; hist of lifetime dist; cancer eventually reappear locally?
 #Verify with analytical; Use p_t=p_0(P**t)
+
