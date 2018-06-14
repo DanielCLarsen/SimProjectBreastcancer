824e90d676e8dc7a537e7117bc1d83f1d33fd38e
Daniel Cohen Larsen <daniel.clarsen@gmail.com>
2018-06-14 11:08:20 +0200

    Weee

--- T6.py
+++ T6.py
@@ -10,3 +10,7 @@
 # - Deaths other than cancer happens all the time, and is therefore very important to consider in the statistics
 # - The fact of the matter is, that in reality, people get better from cancer too, and this should be recorded.
 #How may we relax these assumptions?
+# - P-matrix depends on age/deaths from other causes/ability to get better (return to better state).
+# - Add extra state for deaths from other causes, 
+# - Insert values into the P-matrix where there are 0's below the diagonal, for making sure that the women could get well again.
+# - 
