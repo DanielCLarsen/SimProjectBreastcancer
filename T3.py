8739d24fceaf73719cceedd8219c5784affee247
Emil Astrup Wrisberg <emil.wrisberg@hotmail.com>
2018-06-14 14:32:27 +0200

    Finished task 3 code with tests and saved data

--- T3.py
+++ T3.py
@@ -40,10 +40,16 @@ for i in range(0,women):
         t+=1
     survival_times[i]=(len(X)-1)
 mean_emp=np.mean(survival_times)
+var_emp=np.var(survival_times)
+
+# t-test of mean
+Ttest=(mean_emp-E_T)/np.sqrt(var_emp/women)
+p=2*stats.t.cdf(-abs(Ttest),df=women-1)
+
     
 #Until death; hist of lifetime dist; 
 plt.figure(1)
-plt.hist(survival_times,normed=True)
+surv_ret=plt.hist(survival_times,normed=True)
 plt.xlabel("Survival_time")
 plt.ylabel("#Women")
 plt.title("Survival time distribution for 1000 women")
@@ -54,3 +60,20 @@ for i in range(T):
     theory_pdf[i]=life_time_pdf(i,pi,Ps,p0)
 
 plt.plot(time,theory_pdf)
+
+# chi-squared test of distribution
+# The bin partitioning is not integer
+# so we use floor to calc expected frequencies
+def chi2Test_surv(n,bins,pi,Ps,p0):
+    f_obs = [0]*(len(bins)-1)
+    f_exp = [0]*(len(bins)-1)
+    for i in range(0,len(bins)-1):
+        f_obs[i] = n[i]*(bins[i+1]-bins[i])
+        for t in range(int(np.floor(bins[i])),int(np.floor(bins[i+1]))):
+            f_exp[i]+=float(pi*(Ps**t)*p0)
+    chisq, p = stats.chisquare(f_obs,f_exp)
+    return chisq, p 
+
+n = surv_ret[0]
+bins = surv_ret[1]
+chisq_surv, p_surv = chi2Test_surv(n,bins,pi,Ps,p0)
