beade6a8f75f35a58c86fbd5c37d0e2c39e310ed
Emil Astrup Wrisberg <emil.wrisberg@hotmail.com>
2018-06-14 13:16:00 +0200

    Task 5 done, with appropriate mean results

--- T5.py
+++ T5.py
@@ -32,12 +32,13 @@ meanList = [0]*100
 women=200
 varList = [0]*10
 cList = [0]*10
+'''
 for cCalc in range(0,10):
     print("cCalc: ",cCalc)
     for ite in range(0,100):
         survival_times=[0]*women
-        count=-1
-        for j in range(0,women):
+        count=0
+        for i in range(0,women):
             t=0
             X=[0]
             while X[-1]!=4:
@@ -46,51 +47,53 @@ for cCalc in range(0,10):
                     if cdf[X[t],j-1]<U and U<=cdf[X[t],j]: #Crude check
                         X.append(j-1)
                 t+=1
-            survival_times[count]=(len(X)-1)
+            survival_times[i]=len(X)-1
             if len(X)<=351:
                 count+=1
         #Fraction:
-        deathFraction = count/women
-        deathFracList[ite] = deathFraction
+        deathFracList[ite] = float(count)/women
         #Mean surv:
         meanList[ite] = np.mean(survival_times)
-        varList[cCalc] = np.var(meanList)
-        covariance = np.cov(deathFracList,meanList)      
-        cList[cCalc] = covariance[0,1]/varList[cCalc]
+    varList[cCalc] = np.var(meanList)
+    covariance = np.cov(deathFracList,meanList)      
+    cList[cCalc] = covariance[0,1]/varList[cCalc]
         
 
 cMean = np.mean(cList) #0.0026699819530815235
+'''
 
 #Now we calculate all the other values for another data set than c has been calculated on.
 #Lets calculate Z
-for ite in range(0,100):
-    survival_times=[0]*women
-    count=-1
-    for j in range(0,women):
-        t=0
-        X=[0]
-        while X[-1]!=4:
-            U=np.random.uniform(0,1)
-            for j in range(1,n+1):
-                if cdf[X[t],j-1]<U and U<=cdf[X[t],j]: #Crude check
-                    X.append(j-1)
-            t+=1
-        survival_times[count]=(len(X)-1)
-        if len(X)<=351:
-            count+=1
-    #Fraction:
-    deathFraction = count/women
-    deathFracList[ite] = deathFraction
-    #Mean surv:
-    meanList[ite] = np.mean(survival_times)
 
+for ite in range(0,100):
+        survival_times=[0]*women
+        count=0
+        for i in range(0,women):
+            t=0
+            X=[0]
+            while X[-1]!=4:
+                U=np.random.uniform(0,1)
+                for j in range(1,n+1):
+                    if cdf[X[t],j-1]<U and U<=cdf[X[t],j]: #Crude check
+                        X.append(j-1)
+                t+=1
+            survival_times[i]=len(X)-1
+            if len(X)<=351:
+                count+=1
+        #Fraction:
+        deathFracList[ite] = float(count)/women
+        #Mean surv:
+        meanList[ite] = np.mean(survival_times)
+
+cMean = 0.0026699819530815235
 Z = [0]*100
 for i in range(0,len(Z)):
-    Z[i] = deathFracList[i] + 0.0026699819530815235*(meanList[i]-E_T)
+    Z[i] = deathFracList[i] + cMean*(meanList[i]-E_T)
     
 varDeathFrac = np.var(deathFracList) #0.00103
 varZ = np.var(Z) #0.00254
-#variance of variate is higher than crude... doesnt make sense.
+varRed=(1-varZ/varDeathFrac)*100 # in percentage
+#variance using control variate is smaller
 
 
 
